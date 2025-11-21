8d88823e91963a2ab2702707aa65615c7530ab91
jiregna worku <jiregna123w@gmail.com>
2025-11-22 00:47:15 +0300

    Force rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     base: "/Personal-portfolio/",
     plugins: [react()],
     // Make environment variables available to the client
     define: {
-      'process.env': {}
+      "process.env": {},
     },
     // Expose environment variables that start with VITE_
     server: {
